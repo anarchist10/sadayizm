9b2535e836c9d6a171eb5098d220567b37e03e8e
anarchist <maxi_200914@hotmail.com>
2025-06-25 12:49:42 -0300

    TEST ELO FACEIT

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from 'next/head';
 import Starfield from '../components/Starfield';
+import { useEffect, useState } from 'react';
 
 const nicks = [
   { name: 'anar', url: 'https://steamcommunity.com/id/anrz/' },
@@ -10,6 +11,19 @@ const nicks = [
 ];
 
 export default function Home() {
+  const [elo, setElo] = useState(null);
+
+  useEffect(() => {
+    // Llamada a la API para obtener el ELO de Faceit de 'anar'
+    fetch('https://api.jakobkristensen.com/76561198860541170/{{elo}}[[America/Argentina/Buenos_Aires]]')
+      .then(res => res.json())
+      .then(data => {
+        // Suponiendo que el ELO viene como un número directamente
+        setElo(data.elo || data);
+      })
+      .catch(() => setElo('N/A'));
+  }, []);
+
   return (
     <>
       <Head>
@@ -21,15 +35,32 @@ export default function Home() {
         <div className="gothic-title">sadayizm</div>
         <div className="nick-list">
           {nicks.map(nick => (
-            <a
-              key={nick.name}
-              className="nick-link"
-              href={nick.url}
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              {nick.name}
-            </a>
+            <div key={nick.name} style={{ display: 'flex', alignItems: 'center', gap: '0.5rem' }}>
+              <a
+                className="nick-link"
+                href={nick.url}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                {nick.name}
+              </a>
+              {nick.name === 'anar' && (
+                <>
+                  <span style={{ fontSize: '1.2rem' }}>{elo !== null ? elo : '...'}</span>
+                  <a
+                    href="https://www.faceit.com/es/players/ANARCHlST"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    style={{ display: 'flex', alignItems: 'center' }}
+                  >
+                    {/* Icono Faceit SVG */}
+                    <svg width="20" height="20" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
+                      <path d="M2 28L30 16L2 4L7.5 16L2 28Z" fill="#FF5500"/>
+                    </svg>
+                  </a>
+                </>
+              )}
+            </div>
           ))}
         </div>
       </div>
