adcda9e298e01c2f60293251fbc50508d4a72621
anarchist10 <156141756+anarchist10@users.noreply.github.com>
2025-06-29 18:47:02 -0300

    Implementar diferencia de elo diario con flechas

--- pages/index.js
+++ pages/index.js
@@ -3,25 +3,57 @@ import Starfield from '../components/Starfield';
 import { useEffect, useState, useRef } from 'react';
 
 const nicks = [
-  { name: 'anar', url: 'https://steamcommunity.com/id/anrz/', faceitApi: 'https://api.jakobkristensen.com/76561198860541170/{{elo}}[[America/Argentina/Buenos_Aires]]', faceitUrl: 'https://www.faceit.com/es/players/ANARCHlST' },
-  { name: 'nikito', url: 'https://steamcommunity.com/id/nkto/', faceitApi: 'https://api.jakobkristensen.com/76561198402344265/{{elo}}[[America/Argentina/Buenos_Aires]]', faceitUrl: 'https://www.faceit.com/es/players/nikito' },
-  { name: 'nyoh', url: 'https://steamcommunity.com/id/srz1/', faceitApi: 'https://api.jakobkristensen.com/76561198374148982/{{elo}}[[America/Argentina/Buenos_Aires]]', faceitUrl: 'https://www.faceit.com/es/players/kyrgios' },
-  { name: 'rks', url: 'https://steamcommunity.com/id/5t9/', faceitApi: 'https://api.jakobkristensen.com/76561198023120655/{{elo}}[[America/Argentina/Buenos_Aires]]', faceitUrl: 'https://www.faceit.com/es/players/bendecido' },
-  { name: 'angry', url: 'https://steamcommunity.com/id/69qui9uwjr9qjq9124u1925u15/', faceitApi: 'https://api.jakobkristensen.com/76561198131602113/{{elo}}[[America/Argentina/Buenos_Aires]]', faceitUrl: 'https://www.faceit.com/es/players/oilrigplayer' },
-  { name: 'Supr3me', url: 'https://steamcommunity.com/id/Supr3me76561198063990435/', faceitApi: 'https://api.jakobkristensen.com/76561198063990435/{{elo}}[[America/Argentina/Buenos_Aires]]', faceitUrl: 'https://www.faceit.com/es/players/Supr3me' },
-  { name: 'daker', url: 'https://steamcommunity.com/id/pierdotodo', faceitApi: 'https://api.jakobkristensen.com/76561199108305712/{{elo}}[[America/Argentina/Buenos_Aires]]', faceitUrl: 'https://www.faceit.com/es/players/daker' },
-  { name: 'ElComba', url: 'https://steamcommunity.com/id/combademon666', faceitApi: 'https://api.jakobkristensen.com/76561199027855096/{{elo}}[[America/Argentina/Buenos_Aires]]', faceitUrl: 'https://www.faceit.com/es/players/BRBRCOMBAPIM' },
-  { name: 'Gordoreally', url: 'https://steamcommunity.com/id/lilitacarriooo/', faceitApi: 'https://api.jakobkristensen.com/76561198318387050/{{elo}}[[America/Argentina/Buenos_Aires]]', faceitUrl: 'https://www.faceit.com/es/players/GordoReally' },
-  { name: 'diego2570', url: 'https://steamcommunity.com/id/257O/', faceitApi: 'https://api.jakobkristensen.com/76561198999382443/{{elo}}[[America/Argentina/Buenos_Aires]]', faceitUrl: 'https://www.faceit.com/es/players/goa1221' },
+  { name: 'anar', url: 'https://steamcommunity.com/id/anrz/', faceitApi: 'https://api.jakobkristensen.com/76561198860541170/{{elo}}[[America/Argentina/Buenos_Aires]]', faceitUrl: 'https://www.faceit.com/es/players/ANARCHlST', eloDiffApi: 'https://api.jakobkristensen.com/76561198860541170/{{todayEloDiff}}[[America/Argentina/Buenos_Aires]]' },
+  { name: 'nikito', url: 'https://steamcommunity.com/id/nkto/', faceitApi: 'https://api.jakobkristensen.com/76561198402344265/{{elo}}[[America/Argentina/Buenos_Aires]]', faceitUrl: 'https://www.faceit.com/es/players/nikito', eloDiffApi: 'https://api.jakobkristensen.com/76561198402344265/{{todayEloDiff}}[[America/Argentina/Buenos_Aires]]' },
+  { name: 'nyoh', url: 'https://steamcommunity.com/id/srz1/', faceitApi: 'https://api.jakobkristensen.com/76561198374148982/{{elo}}[[America/Argentina/Buenos_Aires]]', faceitUrl: 'https://www.faceit.com/es/players/kyrgios', eloDiffApi: 'https://api.jakobkristensen.com/76561198374148982/{{todayEloDiff}}[[America/Argentina/Buenos_Aires]]' },
+  { name: 'rks', url: 'https://steamcommunity.com/id/5t9/', faceitApi: 'https://api.jakobkristensen.com/76561198023120655/{{elo}}[[America/Argentina/Buenos_Aires]]', faceitUrl: 'https://www.faceit.com/es/players/bendecido', eloDiffApi: 'https://api.jakobkristensen.com/76561198023120655/{{todayEloDiff}}[[America/Argentina/Buenos_Aires]]' },
+  { name: 'angry', url: 'https://steamcommunity.com/id/69qui9uwjr9qjq9124u1925u15/', faceitApi: 'https://api.jakobkristensen.com/76561198131602113/{{elo}}[[America/Argentina/Buenos_Aires]]', faceitUrl: 'https://www.faceit.com/es/players/oilrigplayer', eloDiffApi: 'https://api.jakobkristensen.com/76561198131602113/{{todayEloDiff}}[[America/Argentina/Buenos_Aires]]' },
+  { name: 'Supr3me', url: 'https://steamcommunity.com/id/Supr3me76561198063990435/', faceitApi: 'https://api.jakobkristensen.com/76561198063990435/{{elo}}[[America/Argentina/Buenos_Aires]]', faceitUrl: 'https://www.faceit.com/es/players/Supr3me', eloDiffApi: 'https://api.jakobkristensen.com/76561198063990435/{{todayEloDiff}}[[America/Argentina/Buenos_Aires]]' },
+  { name: 'daker', url: 'https://steamcommunity.com/id/pierdotodo', faceitApi: 'https://api.jakobkristensen.com/76561199108305712/{{elo}}[[America/Argentina/Buenos_Aires]]', faceitUrl: 'https://www.faceit.com/es/players/daker', eloDiffApi: 'https://api.jakobkristensen.com/76561199108305712/{{todayEloDiff}}[[America/Argentina/Buenos_Aires]]' },
+  { name: 'ElComba', url: 'https://steamcommunity.com/id/combademon666', faceitApi: 'https://api.jakobkristensen.com/76561199027855096/{{elo}}[[America/Argentina/Buenos_Aires]]', faceitUrl: 'https://www.faceit.com/es/players/BRBRCOMBAPIM', eloDiffApi: 'https://api.jakobkristensen.com/76561199027855096/{{todayEloDiff}}[[America/Argentina/Buenos_Aires]]' },
+  { name: 'Gordoreally', url: 'https://steamcommunity.com/id/lilitacarriooo/', faceitApi: 'https://api.jakobkristensen.com/76561198318387050/{{elo}}[[America/Argentina/Buenos_Aires]]', faceitUrl: 'https://www.faceit.com/es/players/GordoReally', eloDiffApi: 'https://api.jakobkristensen.com/76561198318387050/{{todayEloDiff}}[[America/Argentina/Buenos_Aires]]' },
+  { name: 'diego2570', url: 'https://steamcommunity.com/id/257O/', faceitApi: 'https://api.jakobkristensen.com/76561198999382443/{{elo}}[[America/Argentina/Buenos_Aires]]', faceitUrl: 'https://www.faceit.com/es/players/goa1221', eloDiffApi: 'https://api.jakobkristensen.com/76561198999382443/{{todayEloDiff}}[[America/Argentina/Buenos_Aires]]' },
 ];
+
+// Componente para la flecha de diferencia de elo
+function EloDiffArrow({ diff }) {
+  if (diff === 0 || diff === undefined || diff === null) return null;
+  
+  const isPositive = diff > 0;
+  const color = isPositive ? '#00ff00' : '#ff0000';
+  const rotation = isPositive ? '0deg' : '180deg';
+  
+  return (
+    <div style={{ 
+      display: 'flex', 
+      alignItems: 'center', 
+      gap: '0.3rem',
+      fontSize: '0.9rem',
+      color: color
+    }}>
+      <svg 
+        width="12" 
+        height="12" 
+        viewBox="0 0 24 24" 
+        fill={color}
+        style={{ transform: `rotate(${rotation})` }}
+      >
+        <path d="M12 2L22 12H17V22H7V12H2L12 2Z"/>
+      </svg>
+      <span>{isPositive ? '+' : ''}{diff}</span>
+    </div>
+  );
+}
 
 export default function Home() {
   const [elos, setElos] = useState({});
+  const [eloDiffs, setEloDiffs] = useState({});
   const [sortedNicks, setSortedNicks] = useState(nicks);
   const audioRef = useRef(null);
   const startedRef = useRef(false);
 
   useEffect(() => {
+    // Fetch elos actuales
     nicks.forEach(nick => {
       if (nick.faceitApi) {
         fetch(nick.faceitApi)
@@ -32,6 +64,20 @@ export default function Home() {
             setElos(prev => ({ ...prev, [nick.name]: eloValue }));
           })
           .catch(() => setElos(prev => ({ ...prev, [nick.name]: 'N/A' })));
+      }
+    });
+
+    // Fetch diferencias de elo del día
+    nicks.forEach(nick => {
+      if (nick.eloDiffApi) {
+        fetch(nick.eloDiffApi)
+          .then(res => res.json())
+          .then(data => {
+            const diffValue = data.todayEloDiff || data;
+            console.log(`${nick.name} elo diff:`, diffValue, typeof diffValue);
+            setEloDiffs(prev => ({ ...prev, [nick.name]: diffValue }));
+          })
+          .catch(() => setEloDiffs(prev => ({ ...prev, [nick.name]: 0 })));
       }
     });
   }, []);
@@ -139,6 +185,7 @@ export default function Home() {
                 {nick.faceitApi && (
                   <>
                     <span style={{ fontSize: '1.2rem' }}>{elos[nick.name] !== undefined ? elos[nick.name] : '...'}</span>
+                    <EloDiffArrow diff={eloDiffs[nick.name]} />
                     <a
                       href={nick.faceitUrl}
                       target="_blank"
